2c040b70d1f25836354b577c2227198a1d061981
hllamon2301 <150685068+hllamon2301@users.noreply.github.com>
2023-12-11 14:48:06 +0100

    Delete Python tarea 7 directory

--- Python tarea 7/tarea7.2Py.py
+++
@@ -1,8 +0,0 @@
-#Suma de 10 valores
-#Hugo Llamas Montes
-#11/12/2023 (Última edición)
-S=0
-for x in range(10):
-    N=int(input("Introduce un valor para sumar"))
-    S=S+N
-print("La suma es", S)
